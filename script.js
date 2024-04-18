02f90b43b3db6c652e05547652bfb814c3582430
micx83 <166127123+micx83@users.noreply.github.com>
2024-04-18 11:05:18 +0000

    mod funzionanti

--- script.js
+++ script.js
@@ -1,57 +1,26 @@
-var selectedElement = null;
-var isResizing = false;
-var isResizingText = false;
-var resizeDirection = '';
+$(document).ready(function() {
+    var textElement = $('#textElement');
+    var fontElement = $('#font');
+    var baseElement = $('#base');
+    var pendantElement = $('#pendant');
+    var textOutputElement = $('<div id="textOutput"></div>');
 
-var textElement = document.getElementById('textElement'); // Cambiato da 'text' a 'textElement'
-var baseElement = document.getElementById('base');
-var pendantElement = document.getElementById('pendant');
+    textElement.on('input', function() {
+        textOutputElement.text(textElement.val());
+        textOutputElement.css('font-family', fontElement.val());
+        textOutputElement.appendTo(pendantElement);
+    });
 
-textElement.addEventListener('input', function() {
-    pendantElement.textContent = textElement.value;
-});
+    baseElement.on('change', function() {
+        pendantElement.css('background-image', 'url(' + baseElement.val() + ')');
+    });
 
-baseElement.addEventListener('change', function() {
-    pendantElement.style.backgroundImage = 'url(' + baseElement.value + ')';
-});
+    // Aggiungi qui il codice per spostare, ridimensionare ed eliminare il testo
+    textOutputElement.draggable();
+    textOutputElement.resizable();
 
-textElement.addEventListener('mousedown', function(e) {
-    selectedElement = textElement;
-    if (e.offsetX > textElement.offsetWidth - 10) {
-        isResizing = true;
-        resizeDirection = 'x';
-    }
-    else if (e.offsetY > textElement.offsetHeight - 10) {
-        isResizing = true;
-        resizeDirection = 'y';
-    }
-    if (e.offsetX > textElement.offsetWidth - 10 && e.offsetY > textElement.offsetHeight - 10) {
-        isResizingText = true;
-    }
-});
-
-document.addEventListener('mousemove', function(e) {
-    if (selectedElement) {
-        if (isResizing) {
-            if (resizeDirection === 'x') {
-                var newWidth = e.clientX - selectedElement.getBoundingClientRect().left;
-                selectedElement.style.width = newWidth + 'px';
-            }
-            else if (resizeDirection === 'y') {
-                var newHeight = e.clientY - selectedElement.getBoundingClientRect().top;
-                selectedElement.style.height = newHeight + 'px';
-            }
-        }
-        if (isResizingText) {
-            var newFontSize = e.clientY - selectedElement.getBoundingClientRect().top;
-            selectedElement.style.fontSize = newFontSize + 'px';
-        }
-    }
-});
-
-document.addEventListener('mouseup', function() {
-    isResizing = false;
-    isResizingText = false;
-    resizeDirection = '';
-    selectedElement = null;
+    // Elimina il testo quando si fa clic su di esso
+    textOutputElement.on('click', function() {
+        $(this).remove();
+    });
 });
