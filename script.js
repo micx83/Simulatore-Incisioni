db27acace7eb17170b5cd224424f8f9fc8dbf979
micx83 <166127123+micx83@users.noreply.github.com>
2024-04-19 19:21:40 +0000

    mod

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ $(document).ready(function() {
         var textOutputElement = $('<div class="textOutput"></div>');
         textOutputElement.text(textElement.val());
         textOutputElement.css('font-family', fontElement.val());
+        textOutputElement.css('font-size', '16px'); // Imposta la dimensione del font a 16px
         textOutputElement.css('width', 'auto');
         textOutputElement.css('height', 'auto');
 
@@ -32,12 +33,47 @@ $(document).ready(function() {
 
         textOutputElement.appendTo(pendantElement);
         textOutputElement.draggable({ containment: "parent" }); // Contenimento all'interno dell'elemento genitore
-        textOutputElement.resizable({
-            handles: 'se',
-            resize: function() {
-                var scale = Math.sqrt(textOutputElement.width() * textOutputElement.height()) / 2;
-                textOutputElement.css('font-size', scale + 'px');
-            }
+
+        // Crea un elemento div per il handle di ridimensionamento
+        var resizeHandle = $('<div></div>');
+        resizeHandle.css({
+            'width': '10px',
+            'height': '10px',
+            'background': 'red',
+            'position': 'absolute',
+            'right': '0',
+            'bottom': '0',
+            'cursor': 'se-resize'
+        });
+        textOutputElement.append(resizeHandle);
+
+        // Variabili per tenere traccia dello stato del ridimensionamento
+        var isResizing = false;
+        var lastDownX = 0;
+        var lastDownY = 0;
+
+        // Evento mousedown per iniziare il ridimensionamento
+        resizeHandle.on('mousedown', function(e) {
+            isResizing = true;
+            lastDownX = e.clientX;
+            lastDownY = e.clientY;
+        });
+
+        // Evento mousemove per effettuare il ridimensionamento
+        $(window).on('mousemove', function(e) {
+            if (!isResizing) 
+                return;
+            var offsetX = e.clientX - lastDownX;
+            var offsetY = e.clientY - lastDownY;
+            textOutputElement.css('width', (offsetX + textOutputElement.width()) + 'px');
+            textOutputElement.css('height', (offsetY + textOutputElement.height()) + 'px');
+            lastDownX = e.clientX;
+            lastDownY = e.clientY;
+        });
+
+        // Evento mouseup per terminare il ridimensionamento
+        $(window).on('mouseup', function(e) {
+            isResizing = false;
         });
     });
 });
