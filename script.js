a2a55bd3bb2754b11c1d7b9f67980d7f58994734
micx83 <166127123+micx83@users.noreply.github.com>
2024-04-24 17:14:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,43 +5,67 @@ $(document).ready(function() {
     var pendantElement = $('#pendant');
     var placeholderElement = $('#placeholder');
     var zIndex = 0; 
+    var currentTextOutputElement = null;
+    var fontSelectorCopy = null;
 
-    baseElement.on('change', function() {
-        if (baseElement.val()) {
-            var img = new MarvinImage();
-            img.load(baseElement.val(), function() {
-                // Convert the image to grayscale
-                Marvin.grayScale(img, img);
+    var ddData = [
+        {
+            text: "Charm Cuore",
+            value: "images/CHFamilyN.png",
+            imageSrc: "images/CHFamilyN_thumbnail.jpg"
+        },
+        {
+            text: "Ciondolo Cuore",
+            value: "images/CLCFamilyN.png",
+            imageSrc: "images/CLCFamilyN_thumbnail.jpg"
+        },
+        {
+            text: "Ciondolo Tondo",
+            value: "images/CLTFamilyN.png",
+            imageSrc: "images/CLTFamilyN_thumbnail.jpg"
+        },
+        {
+            text: "Ciondolo Uomo",
+            value: "images/CLUFamilyN.png",
+            imageSrc: "images/CLUFamilyN_thumbnail.jpg"
+        },
+        
+    ];
 
-                // Find the edges of the image
-                Marvin.prewitt(img, img);
-
-                // Find the width of the jewel image
-                var maxWidth = findMaxWidth(img);
-
-                // Set the maximum width of the text to the width of the jewel image
-                textElement.css('max-width', maxWidth + 'px');
-
-                pendantElement.css('background-image', 'url(' + baseElement.val() + ')');
+    baseElement.ddslick({
+        data: ddData,
+        width: 300,
+        imagePosition: "left",
+        onSelected: function(selectedData){
+            if (selectedData.selectedData.value) {
+                pendantElement.css('background-image', 'url(' + selectedData.selectedData.value + ')');
                 placeholderElement.hide();
-            });
-        } else {
-            pendantElement.css('background-image', '');
-            placeholderElement.show();
-        }
+            } else {
+                pendantElement.css('background-image', '');
+                placeholderElement.show();
+            }
+        }   
     });
+
 
     $('#engravingForm').on('submit', function(e) {
         e.preventDefault();
     
         var textOutputElement = $('<div class="textOutput"></div>');
         textOutputElement.html(textElement.val().replace(/\n/g, '<br/>')); 
-    
+        textElement.val('');
         textOutputElement.css('font-family', fontElement.val());
         textOutputElement.css('font-size','18'); 
         textOutputElement.css('z-index', zIndex++); 
         textOutputElement.css('position', 'absolute'); 
-    
+
+        textOutputElement.on('click', function() {
+            currentTextOutputElement = $(this);
+            if (fontSelectorCopy) {
+                fontSelectorCopy.val(currentTextOutputElement.css('font-family'));
+            }
+        });
+
         textOutputElement.resizable({
             start: function(event, ui) {
                 $(this).data('startFontSize', parseInt($(this).css('font-size')));
@@ -51,23 +75,13 @@ $(document).ready(function() {
             resize: function(event, ui) {
                 var widthChange = ui.size.width - $(this).data('startWidth');
                 var heightChange = ui.size.height - $(this).data('startHeight');
-        
                 var newFontSize = $(this).data('startFontSize') + Math.min(widthChange, heightChange) / 2;
-
-                // Limit the font size based on the width of the image
-                var maxWidth = parseInt(textElement.css('max-width'));
-                newFontSize = Math.min(newFontSize, maxWidth / $(this).text().length);
-
                 $(this).css({
                     'font-size': newFontSize + 'px',
                     height: 'auto',
                     width: 'auto'
                 });
             }
-        });
-
-        fontElement.on('change', function() {
-            $('.textOutput').css('font-family', fontElement.val());
         });
 
         textOutputElement.draggable({
@@ -78,6 +92,14 @@ $(document).ready(function() {
         closeButton.on('click', function(e) {
             e.stopPropagation();
             textOutputElement.remove();
+ // Check if there are any textOutput elements left
+ if ($('.textOutput').length === 0) {
+    // If not, remove the font selector copy
+    fontSelectorCopy.remove();
+    fontSelectorCopy = null;
+}
+
+
         });
 
         closeButton.css({
@@ -88,17 +110,21 @@ $(document).ready(function() {
 
         textOutputElement.append(closeButton);
         textOutputElement.appendTo(pendantElement);
+
+        if (!fontSelectorCopy) {
+            fontSelectorCopy = $('#font').clone().attr('id', 'fontCopy');
+            fontSelectorCopy.css({
+                position: 'absolute',
+                left: $('#pendant').offset().left - fontSelectorCopy.outerWidth(),
+                top: $('#pendant').offset().top
+            });
+            fontSelectorCopy.appendTo('body');
+            fontSelectorCopy.on('change', function() {
+                if (currentTextOutputElement) {
+                    currentTextOutputElement.css('font-family', fontSelectorCopy.val());
+                }
+            });
+        }
     });
 });
 
-function findMaxWidth(img) {
-    var maxWidth = 0;
-    for (var y = 0; y < img.getHeight(); y++) {
-        for (var x = 0; x < img.getWidth(); x++) {
-            if (img.getIntColor(x, y) != 0) {
-                maxWidth = Math.max(maxWidth, x);
-            }
-        }
-    }
-    return maxWidth;
-}
