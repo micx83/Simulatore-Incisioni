6fd895072ca25b4f68771244be5f671983ab11f5
micx83 <166127123+micx83@users.noreply.github.com>
2024-05-06 13:08:34 +0000

    mod

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ $(document).ready(function() {
     var baseElement = $('#base');
     var pendantElement = $('#pendant');
     var placeholderElement = $('#placeholder');
+    var imageSelectionElement = $('#imageSelection');
     var zIndex = 0; 
     var currentTextOutputElement = null;
     var fontSelectorCopy = null;
+
+    pendantElement.css({
+        'position': 'relative'
+    });
 
     var ddData = [
         {
@@ -29,13 +34,13 @@ $(document).ready(function() {
             value: "images/CLUFamilyN.png",
             imageSrc: "images/CLUFamilyN.png"
         },
-        
     ];
 
     baseElement.ddslick({
         data: ddData,
-        width: 300,
+        width: 200,
         imagePosition: "left",
+        selectText: "Scegli la Base",
         onSelected: function(selectedData){
             if (selectedData.selectedData.value) {
                 pendantElement.css('background-image', 'url(' + selectedData.selectedData.value + ')');
@@ -45,10 +50,74 @@ $(document).ready(function() {
                 placeholderElement.show();
             }
         } 
-        
     });
 
+    var imageSelectionData = [
+        {
+            text: "Omino",
+            value: "grafic/omino1.png",
+            imageSrc: "grafic/omino1.png"
+        },
+        {
+            text: "Donnina",
+            value: "grafic/donnina.png",
+            imageSrc: "grafic/donnina.png"
+        },
+        // Add more images here...
+    ];
 
+    var imageSelectorLabel = $('<p>').text('Vuoi creare un Disegno Personalizzato?').css({
+        'font-size': '12px', // rendi il testo più piccolo
+        'text-align': 'center' // centra il testo
+    }).attr('id', 'imageSelectorLabel'); // aggiungi un id all'elemento p per poterlo rimuovere facilmente in seguito
+    
+    imageSelectorLabel.insertBefore(imageSelectionElement); // inserisci il testo prima del selettore di immagini
+
+    imageSelectionElement.ddslick({
+        data: imageSelectionData,
+        width: 200,
+        imagePosition: "left",
+        selectText: "Scegli gli Elementi",
+        onSelected: function(selectedData){
+            // Aggiungi l'ID all'elemento generato da ddslick
+            $('.dd-container').attr('id', 'imageSelection');
+    
+            if (selectedData.selectedData.value) {
+                var img = $('<img class="selectedImage" src="' + selectedData.selectedData.value + '">').css({
+                    'width': '100%',
+                    'height': '100%'
+                });
+            
+                var imgElement = $('<div class="imageContainer"></div>').css({
+                    'width': '50px',
+                    'height': '50px',
+                    'position': 'absolute'
+                }).append(img).append('<div class="closeButton">X</div>');
+            
+                imgElement.css({
+                    'z-index': zIndex++
+                });
+            
+                imgElement.draggable({
+                    containment: "#pendant"
+                });
+            
+                imgElement.find('.closeButton').click(function() {
+                    imgElement.remove();
+                });
+            
+                // Rendi ridimensionabile l'elemento div che contiene l'immagine
+                imgElement.resizable({
+                    aspectRatio: true,
+                    minHeight: 30,
+                    minWidth: 30
+                });
+            
+                pendantElement.append(imgElement);
+            }
+        } 
+    });
+    
     $('#engravingForm').on('submit', function(e) {
         e.preventDefault();
     
@@ -85,6 +154,7 @@ $(document).ready(function() {
             }
         });
 
+
         textOutputElement.draggable({
             containment: "#pendant"
         });
@@ -96,6 +166,7 @@ $(document).ready(function() {
             if ($('.textOutput').length === 0) {
                 fontSelectorCopy.remove();
                 fontSelectorCopy = null;
+                $('#fontLabel').remove();
             }
         });
 
@@ -112,15 +183,26 @@ $(document).ready(function() {
             fontSelectorCopy = $('#font').clone().attr('id', 'fontCopy');
             fontSelectorCopy.css({
                 position: 'absolute',
-                left: $('#pendant').offset().left - fontSelectorCopy.outerWidth(),
-                top: $('#pendant').offset().top
+                right: $('#pendant').offset().left - fontSelectorCopy.outerWidth(),
+                top: $('#pendant').offset().top + 20
             });
+        
+            var fontLabel = $('<p>').text('Modifica il font').css({
+                position: 'absolute',
+                right: $('#pendant').offset().left - fontSelectorCopy.outerWidth(),
+                top: $('#pendant').offset().top - 10, // posiziona il testo 15px sopra fontSelectorCopy
+                'font-size': '12px' // rendi il testo più piccolo
+            }).attr('id', 'fontLabel'); // aggiungi un id all'elemento p per poterlo rimuovere facilmente in seguito
+        
+            fontLabel.appendTo('body'); // Aggiungi l'elemento p al body
             fontSelectorCopy.appendTo('body');
+        
             fontSelectorCopy.on('change', function() {
                 if (currentTextOutputElement) {
                     currentTextOutputElement.css('font-family', fontSelectorCopy.val());
                 }
             });
         }
+        
     });
 });      
